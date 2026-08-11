07dc23d6c2b32cce21024cf72ba6352d92719380
mohsenmirzazadeh77-dot <mohsen.mirzazadeh77@gmail.com>
2026-08-11 10:23:22 +0330

    Update MainActivity.kt

--- app/src/main/java/com/sarmayeyar/app/MainActivity.kt
+++ app/src/main/java/com/sarmayeyar/app/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
